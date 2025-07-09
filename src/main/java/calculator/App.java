7f11ac334791e69e006cd0c1a8934183fc9ed4b6
Robbie <robbie331@>
2025-07-09 11:28:49 +0900

    init commit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,20 @@
 package calculator;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("num1 = "+num1+", num2 = "+num2);
+
+
 
     }
 }
